295622ab67a716fb67b5bf8f3111cdb0e20dd2b5
Matt <calcifer824@gmail.com>
2024-03-27 14:47:15 -0700

    Initialize Express and Sequelize with CSRF protection

---
+++ backend/routes/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/hot/rod", function (req, res) {
+	res.cookie("XSRF-TOKEN", req.csrfToken());
+	res.send("GODS OF WARRRRRRRRR! May your hammer be mighty.");
+});
+
+router.get("/api/csrf/restore", (req, res) => {
+	const csrfToken = req.csrfToken();
+	res.cookie("XSRF-TOKEN", csrfToken);
+	res.status(200).json({
+		"XSRF-Token": csrfToken,
+	});
+});
+
+module.exports = router;
